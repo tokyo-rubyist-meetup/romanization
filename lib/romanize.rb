4c3e3f6b38b1e689853b3be195c1d5521d0c2f48
廣野 まゆ子 <mayuko.hirono@gmail.com>
2014-03-08 15:17:32 +0900

    iteration

--- lib/romanize.rb
+++ lib/romanize.rb
@@ -1,11 +1,112 @@
 module Romanize
   def romanize(s)
-	if s == 'ひらがな'
-		return 'hiragana'
-	elsif s == 'しんじゅく'
-		return 'sinzyuku'
-	elsif s == 'なっとう'
-		return 'nattou'
-	end
+
+    dictionary = Hash.new    
+    dictionary = {
+'あ' => 'a',
+'い' => 'i',
+'う' => 'u',
+'え' => 'e',
+'お' => 'o',
+'か' => 'ka',
+'き' => 'ki',
+'く' => 'ku',
+'け' => 'ke',
+'こ' => 'ko',
+'さ' => 'sa',
+'し' => 'si',
+'す' => 'su',
+'せ' => 'se',
+'そ' => 'so',
+'た' => 'ta',
+'ち' => 'ti',
+'つ' => 'tu',
+'て' => 'te',
+'と' => 'to',
+'な' => 'na',
+'に' => 'ni',
+'ぬ' => 'nu',
+'ね' => 'ne',
+'の' => 'no',
+'は' => 'ha',
+'ひ' => 'hi',
+'ふ' => 'hu',
+'へ' => 'he',
+'ほ' => 'ho',
+'ま' => 'ma',
+'み' => 'mi',
+'む' => 'mu',
+'め' => 'me',
+'も' => 'mo',
+'や' => 'ya',
+'ゆ' => 'yu',
+'よ' => 'yo',
+'ら' => 'ra',
+'り' => 'ri',
+'る' => 'ru',
+'れ' => 're',
+'ろ' => 'ro',
+'わ' => 'wa',
+'ゐ' => 'wi',
+'ゑ' => 'we',
+'を' => 'wo',
+'ん' => 'n',
+'が' => 'ga',
+'ぎ' => 'gi',
+'ぐ' => 'gu',
+'げ' => 'ge',
+'ご' => 'go',
+'ざ' => 'za',
+'じ' => 'zi',
+'ず' => 'zu',
+'ぜ' => 'ze',
+'ぞ' => 'zo',
+'だ' => 'da',
+'ぢ' => 'di',
+'づ' => 'du',
+'で' => 'de',
+'ど' => 'do',
+'ば' => 'ba',
+'び' => 'bi',
+'ぶ' => 'bu',
+'べ' => 'be',
+'ぼ' => 'bo',
+'ぱ' => 'pa',
+'ぴ' => 'pi',
+'ぷ' => 'pu',
+'ぺ' => 'pe',
+'ぽ' => 'po',
+'きゃ'=> 'kya', 'きゅ'=> 'kyu', 'きょ'=> 'kyo',
+'しゃ'=> 'sya', 'しゅ'=> 'syu', 'しょ'=> 'syo',
+'ちゃ'=> 'tya', 'ちゅ'=> 'tyu', 'ちょ'=> 'tyo',
+'にゃ'=> 'nya', 'にゅ'=> 'nyu', 'にょ'=> 'nyo',
+'ひゃ'=> 'hya', 'ひゅ'=> 'hyu', 'ひょ'=> 'hyo',
+'みゃ'=> 'mya', 'みゅ'=> 'myu', 'みょ'=> 'myo',
+'りゃ'=> 'rya', 'りゅ'=> 'ryu', 'りょ'=> 'ryo',
+'ぎゃ'=> 'gya', 'ぎゅ'=> 'gyu', 'ぎょ'=> 'gyo',
+'じゃ'=> 'zya', 'じゅ'=> 'zyu', 'じょ'=> 'zyo',
+'ぢゃ'=> 'dya', 'ぢゅ'=> 'dyu', 'ぢょ'=> 'dyo',
+'びゃ'=> 'bya', 'びゅ'=> 'byu', 'びょ'=> 'byo',
+'ぴゃ'=> 'pya', 'ぴゅ'=> 'pyu', 'ぴょ'=> 'pyo'
+}
+
+    small_letters = %w(ゃ ゅ ょ ぁ ぃ ぅ)
+
+    answer = ""
+    s.each_char.to_a.each_index do |index|
+      #next_letter = s[index+1]
+      #if next_letter 
+      letter = s[index]
+      answer << dictionary[letter]
+    end
+    return answer
+
+  	# if s == 'ひらがな'
+  	# 	return 'hiragana'
+  	# elsif s == 'しんじゅく'
+  	# 	return 'sinzyuku'
+  	# elsif s == 'なっとう'
+  	# 	return 'nattou'
+  	# end
   end
 end
--- spec/romanize_spec.rb
+++ spec/romanize_spec.rb
@@ -1,16 +1,28 @@
 require 'romanize'
 
-describe '#romanize' do
+describe '#romanizeはひらがなをローマ字に変えられる' do
 	include Romanize
-	it 'converts basic hiragana' do
-		expect( romanize('ひらがな') ).to eq('hiragana')
-	end
 
-	it 'converts shu sinzyuku' do
-		expect( romanize('しんじゅく') ).to eq('sinzyuku')
-	end
 
-	it 'converts sokuon sinzyuku' do
-		expect( romanize('なっとう') ).to eq('nattou')
+  context 'ようおんを' do
+  	it 'ローマ字に変えられる' do
+	   	expect( romanize('しんじゅく') ).to eq('sinzyuku')
+    end
+  end
+
+  context 'そくおん' do
+  	it 'ローマ字に変えられる' do
+  		expect( romanize('なっとう') ).to eq('nattou')
+  	end
+  end
+
+	context 'ひらがなを' do
+    it 'ローマ字に変えられる' do
+      expect( romanize('ひらがな') ).to eq('hiragana')
+		end
+
+    it 'ローマ字に変えられる' do
+      expect( romanize('くつした') ).to eq('kutusita')
+    end
 	end
 end
